d7945ef0ad8290f48ad71908d9c857fb4e0d0b03
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-05-25 08:24:07 -0700

    Initial commit

---
+++ src/main/java/com/ravi/microservicesbeerservice/MicroservicesBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.microservicesbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/microservicesbeerservice/MicroservicesBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.microservicesbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
